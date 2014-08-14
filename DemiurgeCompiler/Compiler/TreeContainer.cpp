3817e698b7afa5d2e5842838bbc0dca8805b94c3
traplol <max.mickey1991@gmail.com>
2014-08-13 21:56:12 -0500

    fixed bug with tree container destructor.

--- DemiurgeCompiler/Compiler/TreeContainer.cpp
+++ DemiurgeCompiler/Compiler/TreeContainer.cpp
@@ -1,8 +1,8 @@
 #include "TreeContainer.h"
 
 TreeContainer::~TreeContainer() {
-    while (!ClassDefinitions.empty()) delete ClassDefinitions.back(); ClassDefinitions.pop_back();
-    while (!FunctionDefinitions.empty()) delete FunctionDefinitions.back(); FunctionDefinitions.pop_back();
-    while (!TopLevelExpressions.empty()) delete TopLevelExpressions.back(); TopLevelExpressions.pop_back();
-    while (!ExternalDeclarations.empty()) delete ExternalDeclarations.back(); ExternalDeclarations.pop_back();
+    while (!ClassDefinitions.empty()) delete ClassDefinitions.back(), ClassDefinitions.pop_back();
+    while (!FunctionDefinitions.empty()) delete FunctionDefinitions.back(), FunctionDefinitions.pop_back();
+    while (!TopLevelExpressions.empty()) delete TopLevelExpressions.back(), TopLevelExpressions.pop_back();
+    while (!ExternalDeclarations.empty()) delete ExternalDeclarations.back(), ExternalDeclarations.pop_back();
 }
